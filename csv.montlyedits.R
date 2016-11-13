266464a90bf71b63a422aee2da4663b038bf4cfa
Goggins, Sean Patrick <gogginss@missouri.edu>
2016-11-13 16:03:23 -0500

    ugh

--- csv.montlyedits.R
+++ csv.montlyedits.R
@@ -1,6 +1,8 @@
 library(ggplot2)
+library(zoo)
+
 wsme <- read.csv("women-scientists-monthly-edits.csv", 
-                 colClasses=c(month="character", edits="character"))
+                 colClasses=c(month="character", edits="numeric"))
 
 for (i in nrow(wsme))
 {
@@ -9,7 +11,26 @@ for (i in nrow(wsme))
   
   wsme$theYear <- substr(as.character(wsme$month),1,4)
   wsme$theMonth <- substr(as.character(wsme$month),5,6)
+  predate <- paste(as.numeric(wsme$theYear), "-", as.numeric(wsme$theMonth))
+  
+  #tempy <- c(as.numeric(theMonth),1,as.numeric(theYear))
+  #tempy2 <- as.Date(tempy)          
+  
+  # wsme$date <- as.Date(as.yearmon(paste(as.numeric(wsme$theYear), "-", as.numeric(wsme$theMonth))))
+  wsme$date <- as.Date(as.character(as.numeric(wsme$month)), "%Y%m%d%h%m%s")
+  as.Date(as.character(as.numeric(wsme$month)), "%Y%m%d%h%m%s")
+  
+  wsme$date <- as.Date(format(wsme$month, scientific=F), format="%Y%m%d")
   
 }
 
-qplot(edits, data=wsme, geom="histogram")
+#qplot(date, data=wsme, weight = edits, geom="histogram", binwidth=1, xlab=theYear)
+
+#ggplot(date, data=wsme,  weight = edits, geom="histogram", binwidth=1) +
+
+ggplot(data=wsme, mapping= aes(x=date, fill=edits, y=edits), group = date) 
+
++
+  geom_histogram(inherit.aes = TRUE)
+
+
